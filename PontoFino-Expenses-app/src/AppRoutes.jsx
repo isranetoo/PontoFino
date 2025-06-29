db8c7e1f796bf21a37e498976e2bfcb3507ba9ba
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2025-06-28 23:50:23 -0300

    [FIX] Update routing and navigation for improved structure and accessibility

--- PontoFino-Expenses-app/src/AppRoutes.jsx
+++ PontoFino-Expenses-app/src/AppRoutes.jsx
@@ -1,5 +1,3 @@
-import NavBar from './components/Gestão de Orçamento/NavBar';
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
@@ -10,12 +8,6 @@ import Subscriptions from './pages/Subscriptions';
 import Login from './components/Login';
 import Register from './components/Register';
 
-import Dashboard from './components/Gestão de Orçamento/Dashboard';
-import BudgetSettings from './components/Gestão de Orçamento/BudgetSettings';
-import TransactionForm from './components/Gestão de Orçamento/TransactionForm';
-import TransactionList from './components/Gestão de Orçamento/TransactionList';
-import Investments from './components/Gestão de Orçamento/Investments';
-import GoalsManager from './components/Gestão de Orçamento/GoalsManager';
 import SimuladorInvestimentos from './components/Simulador de Investimentos/SimuladorInvestimentos';
 
 export default function AppRoutes() {
@@ -27,16 +19,8 @@ export default function AppRoutes() {
         <Route path="/subscriptions" element={<Subscriptions />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/projetos-futuros" element={<ProjetoFuturo />} />
-        {/* Rotas para Gestão de Orçamento */}
-        <Route path="/orcamento" element={<Dashboard />} />
-        <Route path="/orcamento/configuracoes" element={<BudgetSettings />} />
-        <Route path="/orcamento/nova-transacao" element={<TransactionForm />} />
-        <Route path="/orcamento/transacoes" element={<TransactionList transactions={[]} categories={[]} onDeleteTransaction={()=>{}} />} />
-        <Route path="/orcamento/investimentos" element={<Investments />} />
-        <Route path="/orcamento/metas" element={<GoalsManager goals={[]} onAddGoal={()=>{}} onUpdateGoal={()=>{}} onDeleteGoal={()=>{}} />} />
-        {/* Exemplo de rota para NavBar, ajuste conforme necessário */}
-        <Route path="/orcamento/navbar" element={<NavBar tab={"dashboard"} setTab={()=>{}} />} />
+        <Route path="/projetos-futuros" element={<ProjetoFuturo />} />        {/* Rotas para Gestão de Orçamento */}
+        <Route path="/orcamento" element={<App />} />
         {/* Rota para Simulador de Investimentos */}
         <Route path="/simulador-investimentos" element={<SimuladorInvestimentos />} />
         <Route path="/*" element={<App />} />
